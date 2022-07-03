c26890359cbf0b9e7cac58115c41f27a5c925d6f
magarets <yoonan@LAPTOP-90NAGJ5I.localdomain>
2022-07-03 14:54:20 +0900

    modified

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sum.h"
+
+int main(){
+	int res = sum(10);
+	printf("result = %d\n", res);
+	return 0;
+}
--- sum.cpp
+++ sum.cpp
@@ -1,8 +1,8 @@
-include "sum.h"
+#include "sum.h"
 
 int sum(int n)
 {
 	int res = 0;
-	for(i=1; i<=n; ++i) res+=i;
+	for(int i=1; i<=n; ++i) res+=i;
 	return res;
 }
